9b572c7ae8f7fae1c472b6eb3954273ec669c2eb
Coleson <thenorthernquasar@gmail.com>
2015-11-18 21:16:28 -0800

    Finalized telemetry logic for color sensor testing, added LED toggle functionality.

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/ColorSensorTest.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/ColorSensorTest.java
@@ -2,51 +2,96 @@ package org.usfirst.ftc.exampleteam.yourcodehere;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
 
 import org.swerverobotics.library.SynchronousOpMode;
 import org.swerverobotics.library.interfaces.Autonomous;
+import org.swerverobotics.library.interfaces.IFunc;
+import org.swerverobotics.library.interfaces.TeleOp;
 
 /**
  * Robot starts on blue side, goes to beacon,
  * presses beacon button, and scores climber
  */
-@Autonomous(name="ColorSensorTest")
-public class ColorSensorTest extends SynchronousOpMode
-{
-    // Declare sensors
-    ColorSensor colorSensorBeacon = null;
-    DigitalChannel LED = null;
+@Autonomous(name = "ColorSensorTest")
+public class ColorSensorTest extends SynchronousOpMode {
+    ColorSensor sensorRGB = null;
+    DigitalChannel sensorLED = null;
 
-    @Override public void main() throws InterruptedException
-    {
-        // Initialize sensors
-        colorSensorBeacon = hardwareMap.colorSensor.get("colorSensorBeacon");
-        LED = hardwareMap.digitalChannel.get("LED");
+    final boolean LED_ON = true;
+    final boolean LED_OFF = false;
 
+    @Override
+    public void main() throws InterruptedException {
 
+        // get a reference to our ColorSensor object.
+        sensorRGB = hardwareMap.colorSensor.get("colorSensorBeacon");
+
+        // get a reference to the LED on the color sensor board
+        sensorLED = hardwareMap.digitalChannel.get("LED");
+        sensorLED.setMode(DigitalChannelController.Mode.OUTPUT);
+        sensorLED.setState(LED_OFF);
+
+        // Set up our dashboard computations
+        composeDashboard();
+
+        // Wait until we're told to go
         waitForStart();
 
+        // Loop and update the dashboard
+        while (this.opModeIsActive()) {
 
+            if (this.updateGamepads()) {
+                if (this.gamepad1.a) {
+                    //toggle LED state
+                    sensorLED.setState(!sensorLED.getState());
+                }
+            }
 
-        while(this.opModeIsActive())
-        {
-            FollowLine();
-        }
-    }
-
-    public void FollowLine() throws InterruptedException
-    {
-
-
-        while (opModeIsActive())
-        {
-            this.LED.setState(false);
-            telemetry.addData("red", this.colorSensorBeacon.red());
-            telemetry.addData("green", this.colorSensorBeacon.green());
-            telemetry.addData("blue",  this.colorSensorBeacon.blue());
-            telemetry.addData("alpha", this.colorSensorBeacon.alpha());
             telemetry.update();
-            this.idle();
+
+            idle();
         }
     }
+
+    void composeDashboard() {
+        telemetry.addLine(
+                telemetry.item("loop count: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return getLoopCount();
+                    }
+                }));
+        telemetry.addLine(
+                telemetry.item("color: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return "r: " + sensorRGB.red() + " g: " + sensorRGB.green() + " b: " + sensorRGB.blue();
+                    }
+                }));
+        telemetry.addLine(
+                telemetry.item("led: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return sensorLED.getState();
+                    }
+                }));
+        telemetry.addLine(
+                telemetry.item("Compiled Color: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        if (sensorRGB.blue() >= sensorRGB.red() && sensorRGB.blue() >= sensorRGB.green()) {
+                            return "Blue";
+                        } else if (sensorRGB.red() >= sensorRGB.blue() && sensorRGB.red() >= sensorRGB.green()) {
+                            return "Red";
+                        } else {
+                            return "White or Black";
+                        }
+                    }
+                }));
+
+    }
+
 }
+
+
