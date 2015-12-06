d8a4a08b515b7858382288026e19c7a0cef3035c
mridulavenkatesan <mridulavenkatesan@gmail.com>
2015-12-05 20:49:33 -0800

    The zip line hitters work.

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/ServoTester.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/ServoTester.java
@@ -13,6 +13,8 @@
         // Declare servo
         Servo CollectorServo = null;
         Servo LeftZiplineHitter = null;
+        Servo RightZiplineHitter = null;
+        double RightServoPostion = 0;
 
 
         @Override
@@ -20,6 +22,9 @@
             // Initialize servo
             this.CollectorServo = this.hardwareMap.servo.get("CollectorServo");
             this.LeftZiplineHitter = this.hardwareMap.servo.get("LeftZiplineHitter");
+            this.RightZiplineHitter = this.hardwareMap.servo.get("RightZiplineHitter");
+            RightZiplineHitter.setDirection(Servo.Direction.REVERSE);
+            RightZiplineHitter.setPosition(RightServoPostion);
 
             // Configure dashboard
             this.telemetry.addLine
@@ -40,6 +45,15 @@
                                 }
                             })
                     );
+            this.telemetry.addLine
+                    (
+                            this.telemetry.item("RightZiplineHitter:", new IFunc<Object>() {
+                                @Override
+                                public Object value() {
+                                    return RightZiplineHitter.getPosition();
+                                }
+                            })
+                    );
 
             // Wait until we've been given the ok to go
             this.waitForStart();
@@ -47,7 +61,7 @@
             // Enter a loop processing all the input we receive
             while (this.opModeIsActive()) {
                 if (this.updateGamepads()) {
-                    if (this.gamepad1.a)
+                    if (this.gamepad2.a)
                     {
                         CollectorServo.setPosition(0.0);
                         this.telemetry.update();
@@ -58,17 +72,43 @@
                         this.telemetry.update();
                     }
 
-                    if (this.gamepad1.right_bumper)
+                    if (this.gamepad2.left_bumper)
                     {
                        LeftZiplineHitter.setPosition(90);
                        this.telemetry.update();
                     }
 
-                    if (this.gamepad1.left_bumper)
+                    if ((this.gamepad2.left_trigger > 0.2))
                     {
                         LeftZiplineHitter.setPosition(-90);
                         this.telemetry.update();
                     }
+                    //The RightZiplineHitter reads from (0-1), which is different than the LeftZiplineHitter(0-360)
+                    if (this.gamepad2.right_bumper)
+                    {
+                        RightZiplineHitter.setPosition(0.75);
+                        this.telemetry.update();
+                    }
+
+                    if ((this.gamepad2.right_trigger > 0.2))
+                    {
+                        RightZiplineHitter.setPosition(-0.75);
+                        this.telemetry.update();
+                    }
+                    /*if (this.gamepad1.x)
+                    {
+                        RightServoPostion += 0.05;
+                        RightZiplineHitter.setPosition(RightServoPostion);
+                        this.telemetry.update();
+                    }
+
+                    if ((this.gamepad1.y))
+                    {
+                        RightServoPostion -= 0.05;
+                        RightZiplineHitter.setPosition(RightServoPostion);
+                        this.telemetry.update();
+                    }*/
+
                 }
 
                 /*CollectorServo.setPosition(0);
