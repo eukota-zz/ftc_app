77292923cca39d584055d9d6521aaaa24010d719
Cole <Cole>
2015-12-17 19:50:18 -0800

    We cleaned up the TeleOp program by using lists to make long lists of motors and servos more concise.  The program is now easier to edit because the lists replace the long lines of similar code with for loops over the lists.

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/Synch6220TeleOp.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/Synch6220TeleOp.java
@@ -9,6 +9,9 @@ import org.swerverobotics.library.SynchronousOpMode;
 import org.swerverobotics.library.interfaces.IFunc;
 import org.swerverobotics.library.interfaces.TeleOp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 6220's TeleOp for driving our triangle wheels robot.
  */
@@ -24,7 +27,7 @@ public class Synch6220TeleOp extends SynchronousOpMode
     DcMotor MotorHanger = null;
 
     // Declare servos
-    //Servo CollectorServo = null;
+    //Servo6220 CollectorServo = null;
     Servo LeftZiplineHitter = null;
     Servo RightZiplineHitter = null;
     Servo HikerDropper = null;
@@ -32,17 +35,55 @@ public class Synch6220TeleOp extends SynchronousOpMode
     Servo HolderServoLeft = null;
     Servo HolderServoRight = null;
 
+    List<DcMotor> motors = new ArrayList<>();
+    List<Servo> servos = new ArrayList<>();
+
+
     boolean LeftZiplineHitterDeployed = false;
     boolean RightZiplineHitterDeployed = false;
     boolean HolderServoLeftDeployed = false;
     boolean HolderServoRightDeployed = false;
+
+    public Synch6220TeleOp()
+    {
+        // Initialize our hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names you assigned during the robot configuration
+        // step you did in the FTC Robot Controller app on the phone.
+
+        //dynamically load all servos
+        for (String curServoName : Servo6220.GetNames())
+        {
+            servos.add(this.hardwareMap.servo.get(curServoName));
+        }
+        //initialize old servo references for backwards compatibility
+        this.LeftZiplineHitter = servos.get(Servo6220.LeftZiplineHitter.ordinal());
+        this.RightZiplineHitter = servos.get(Servo6220.RightZiplineHitter.ordinal());
+        this.HikerDropper = servos.get(Servo6220.HikerDropper.ordinal());
+        this.HangerServo = servos.get(Servo6220.HangerServo.ordinal());
+        this.HolderServoLeft = servos.get(Servo6220.HolderServoLeft.ordinal());
+        this.HolderServoRight = servos.get(Servo6220.HolderServoRight.ordinal());
+
+        //dynamically load all motors
+        for (String curMotorName : Motor6220.GetNames())
+        {
+            motors.add(this.hardwareMap.dcMotor.get(curMotorName));
+        }
+        //initialize old motor references for backwards compatibility
+        this.MotorRightBack = motors.get(Motor6220.RightBack.ordinal());
+        this.MotorLeftBack = motors.get(Motor6220.LeftBack.ordinal());
+        this.MotorLeftTriangle = motors.get(Motor6220.RightTriangle.ordinal());
+        this.MotorLeftClimber = motors.get(Motor6220.LeftTriangle.ordinal());
+        this.MotorRightClimber = motors.get(Motor6220.RightClimber.ordinal());
+        this.MotorRightTriangle = motors.get(Motor6220.LeftClimber.ordinal());
+        this.MotorHanger = motors.get(Motor6220.MotorHanger.ordinal());
+    }
 
     enum DriveModeEnum
     {
         DriveModeField,
         DriveModeBackwards,
         DriveModeRamp
-    };
+    }
 
     DriveModeEnum currentDriveMode = DriveModeEnum.DriveModeField;
 
@@ -54,7 +95,8 @@ public class Synch6220TeleOp extends SynchronousOpMode
     static double currentDrivePowerFactor = FULL_POWER;
 
     //the drive wheels are larger than the triangle wheels so we drive them at less power
-    double WHEEL_DRIVE_MULTIPLIER = 0.5;
+    //TODO change this multiplier to a more accurate number, perhaps closer to 0.8
+    double WHEEL_DRIVE_MULTIPLIER = 0.3;
 
     //servo constants
     static final double LEFT_ZIPLINEHITTER_NOTDEPLOYED = -1.0;
@@ -75,8 +117,7 @@ public class Synch6220TeleOp extends SynchronousOpMode
     static final float JOYSTICK_DEADZONE = 0.05f;
 
     @Override
-    protected void main() throws InterruptedException
-    {
+    protected void main() throws InterruptedException {
         //Initialize our hardware
         this.initializeHardware();
 
@@ -87,10 +128,8 @@ public class Synch6220TeleOp extends SynchronousOpMode
         this.waitForStart();
 
         // Enter a loop processing all the input we receive
-        while (this.opModeIsActive())
-        {
-            if (this.updateGamepads())
-            {
+        while (this.opModeIsActive()) {
+            if (this.updateGamepads()) {
                 // There is (likely) new gamepad input available.
                 this.handleDriverInput(this.gamepad1, this.gamepad2);
 
@@ -107,28 +146,13 @@ public class Synch6220TeleOp extends SynchronousOpMode
 
     private void initializeHardware()
     {
-        // Initialize our hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names you assigned during the robot configuration
-        // step you did in the FTC Robot Controller app on the phone.
-        this.MotorRightBack = this.hardwareMap.dcMotor.get("MotorRightBack");
-        this.MotorLeftBack = this.hardwareMap.dcMotor.get("MotorLeftBack");
-        this.MotorLeftTriangle = this.hardwareMap.dcMotor.get("MotorLeftTriangle");
-        this.MotorLeftClimber = this.hardwareMap.dcMotor.get("MotorLeftClimber");
-        this.MotorRightClimber = this.hardwareMap.dcMotor.get("MotorRightClimber");
-        this.MotorRightTriangle = this.hardwareMap.dcMotor.get("MotorRightTriangle");
-        this.MotorHanger = this.hardwareMap.dcMotor.get("MotorHanger");
-
         // Configure the knobs of the hardware according to how you've wired your
         // robot. Here, we assume that there are no encoders connected to the motors,
         // so we inform the motor objects of that fact (we might use encoders later.)
-        this.MotorRightBack.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorLeftBack.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorLeftTriangle.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorLeftClimber.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorRightClimber.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorRightTriangle.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorHanger.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-
+        for (DcMotor curMotor : motors)
+        {
+            curMotor.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        }
 
         //the correct motors should be reversed, since they are on the opposite side of the robot.
         this.MotorRightBack.setDirection(DcMotor.Direction.REVERSE);
@@ -136,14 +160,6 @@ public class Synch6220TeleOp extends SynchronousOpMode
         this.MotorLeftClimber.setDirection(DcMotor.Direction.REVERSE);
 
         stopAllMotors();
-
-        //this.CollectorServo = this.hardwareMap.servo.get("CollectorServo");
-        this.LeftZiplineHitter = this.hardwareMap.servo.get("LeftZiplineHitter");
-        this.RightZiplineHitter = this.hardwareMap.servo.get("RightZiplineHitter");
-        this.HikerDropper = this.hardwareMap.servo.get("HikerDropper");
-        this.HangerServo = this.hardwareMap.servo.get("HangerServo");
-        this.HolderServoLeft = this.hardwareMap.servo.get("HolderServoLeft");
-        this.HolderServoRight = this.hardwareMap.servo.get("HolderServoRight");
 
         RightZiplineHitter.setDirection(Servo.Direction.REVERSE);
 
@@ -154,21 +170,16 @@ public class Synch6220TeleOp extends SynchronousOpMode
         HolderServoLeft.setPosition(HOLDER_SERVO_LEFT_NOTDEPLOYED);
         HolderServoRight.setPosition(HOLDER_SERVO_RIGHT_NOTDEPLOYED);
 
-
         this.gamepad1.setJoystickDeadzone(JOYSTICK_DEADZONE);
         this.gamepad2.setJoystickDeadzone(JOYSTICK_DEADZONE);
     }
 
     private void stopAllMotors()
     {
-        this.MotorRightBack.setPower(0);
-        this.MotorRightClimber.setPower(0);
-        this.MotorRightTriangle.setPower(0);
-        this.MotorLeftBack.setPower(0);
-        this.MotorLeftTriangle.setPower(0);
-        this.MotorLeftClimber.setPower(0);
-        this.MotorHanger.setPower(0);
-
+        for (DcMotor curMotor: this.motors)
+        {
+            curMotor.setPower(0.0);
+        }
     }
 
     /**
@@ -177,54 +188,40 @@ public class Synch6220TeleOp extends SynchronousOpMode
      */
     private void driveForwards(double leftSidePower, double rightSidePower, double leftTrianglePower, double rightTrianglePower)
     {
-        //motor powers
-        double wheelPowerLeft = 0.0;
-        double wheelPowerRight = 0.0;
-        double wheelClimberLeft = 0.0;
-        double wheelClimberRight = 0.0;
-        double trianglePowerLeft = 0.0;
-        double trianglePowerRight = 0.0;
+        this.MotorRightBack.setPower(rightSidePower * WHEEL_DRIVE_MULTIPLIER * currentDrivePowerFactor);
 
-        wheelPowerLeft = leftSidePower * WHEEL_DRIVE_MULTIPLIER * currentDrivePowerFactor;
-        wheelPowerRight = rightSidePower * WHEEL_DRIVE_MULTIPLIER * currentDrivePowerFactor;
+        this.MotorLeftBack.setPower(leftSidePower * WHEEL_DRIVE_MULTIPLIER * currentDrivePowerFactor);
 
-        trianglePowerLeft = leftSidePower * currentDrivePowerFactor;
-        trianglePowerRight = rightSidePower * currentDrivePowerFactor;
+        //TODO triangle vs climber naming is confusing
+        this.MotorRightTriangle.setPower(rightSidePower * currentDrivePowerFactor);
 
-        wheelClimberLeft = leftTrianglePower * currentDrivePowerFactor;
-        wheelClimberRight = rightTrianglePower * currentDrivePowerFactor;
+        this.MotorLeftTriangle.setPower(leftSidePower * currentDrivePowerFactor);
 
-        //set the powers on the motors
-        MotorRightBack.setPower( wheelPowerRight );
-        MotorLeftBack.setPower( wheelPowerLeft );
-        MotorRightTriangle.setPower( trianglePowerRight );
-        MotorLeftTriangle.setPower( trianglePowerLeft );
-        MotorLeftClimber.setPower(  wheelClimberLeft);
-        MotorRightClimber.setPower( wheelClimberRight );
+        this.MotorRightClimber.setPower(rightTrianglePower * currentDrivePowerFactor);
+
+        this.MotorLeftClimber.setPower(leftTrianglePower * currentDrivePowerFactor);
     }
 
     /**
      * This does the same thing as the driveForwards function,
      * but it prepares to drive onto the ramp
-     *
      */
     private void driveBackwards(double leftSidePower, double rightSidePower, double leftTrianglePower, double rightTrianglePower)
     {
-       driveForwards(-1*leftSidePower, -1*rightSidePower, -1*leftTrianglePower, -1*rightTrianglePower);
+        driveForwards(-1 * leftSidePower, -1 * rightSidePower, -1 * leftTrianglePower, -1 * rightTrianglePower);
     }
 
     /**
      * This is the body of the TeleOp that allows the driver to control the robot.
      */
     //TO DO:  we need to finish refactoring this.
-    void driveRobot(Gamepad pad) throws InterruptedException
-    {
+    void driveRobot(Gamepad pad) throws InterruptedException {
         // Remember that the gamepad sticks range from -1 to +1, and that the motor
         // power levels range over the same amount
 
 
         //read input from the controller
-        double  leftSidePower = pad.left_stick_y;
+        double leftSidePower = pad.left_stick_y;
         double rightSidePower = pad.right_stick_y;
         double climberPower = pad.right_trigger * -1;
 
@@ -233,8 +230,7 @@ public class Synch6220TeleOp extends SynchronousOpMode
          */
 
         //field driving mode
-        if (currentDriveMode == DriveModeEnum.DriveModeField)
-        {
+        if (currentDriveMode == DriveModeEnum.DriveModeField) {
             leftSidePower = pad.right_stick_y;
             rightSidePower = pad.left_stick_y;
             climberPower = pad.right_trigger;
@@ -242,13 +238,11 @@ public class Synch6220TeleOp extends SynchronousOpMode
         }
         //"ready" mode for getting ready to climb the ramp
         //we need to drive backwards when aligning with the ramp
-        else if (currentDriveMode == DriveModeEnum.DriveModeBackwards)
-        {
-           driveBackwards(leftSidePower, rightSidePower, climberPower, climberPower);
+        else if (currentDriveMode == DriveModeEnum.DriveModeBackwards) {
+            driveBackwards(leftSidePower, rightSidePower, climberPower, climberPower);
         }
         //drive climb mode
-        else if (currentDriveMode == DriveModeEnum.DriveModeRamp)
-        {
+        else if (currentDriveMode == DriveModeEnum.DriveModeRamp) {
             //since we want both our climbers and wheels to have the same power, we set the climbers equal to the left and right sides
             driveBackwards(leftSidePower, rightSidePower, leftSidePower, rightSidePower);
         }
@@ -260,8 +254,7 @@ public class Synch6220TeleOp extends SynchronousOpMode
         if (pad2.y)
         {
             HikerDropper.setPosition(HIKER_DROPPER_DEPLOYED);
-        }
-        else
+        } else
         {
             HikerDropper.setPosition(HIKER_DROPPER_NOTDEPLOYED);
         }
@@ -269,25 +262,21 @@ public class Synch6220TeleOp extends SynchronousOpMode
         if (pad2.dpad_left)
         {
             HangerServo.setPosition(HANGER_SERVO_DEPLOYED);
-        }
-        else if (pad2.dpad_right)
+        } else if (pad2.dpad_right)
         {
             HangerServo.setPosition(HANGER_SERVO_NOTDEPLOYED);
-        }
-        else
+        } else
         {
             HangerServo.setPosition(HANGER_SERVO_STOP);
         }
 
         if (pad2.dpad_down)
         {
-            MotorHanger.setPower(-1*FULL_POWER);
-        }
-        else if (pad2.dpad_up)
+            MotorHanger.setPower(-1 * FULL_POWER);
+        } else if (pad2.dpad_up)
         {
             MotorHanger.setPower(FULL_POWER);
-        }
-        else
+        } else
         {
             MotorHanger.setPower(STOP);
         }
@@ -297,8 +286,7 @@ public class Synch6220TeleOp extends SynchronousOpMode
         {
             HolderServoRight.setPosition(HOLDER_SERVO_RIGHT_DEPLOYED);
             HolderServoRightDeployed = true;
-        }
-        else if (pad2.b & HolderServoRightDeployed)
+        } else if (pad2.b & HolderServoRightDeployed)
         {
             HolderServoRight.setPosition(HOLDER_SERVO_RIGHT_NOTDEPLOYED);
             HolderServoRightDeployed = false;
@@ -308,8 +296,7 @@ public class Synch6220TeleOp extends SynchronousOpMode
         {
             HolderServoLeft.setPosition(HOLDER_SERVO_LEFT_DEPLOYED);
             HolderServoLeftDeployed = true;
-        }
-        else if (pad2.x & HolderServoLeftDeployed)
+        } else if (pad2.x & HolderServoLeftDeployed)
         {
             HolderServoLeft.setPosition(HOLDER_SERVO_LEFT_NOTDEPLOYED);
             HolderServoLeftDeployed = false;
@@ -329,34 +316,32 @@ public class Synch6220TeleOp extends SynchronousOpMode
             LeftZiplineHitter.setPosition(LEFT_ZIPLINEHITTER_DEPLOYED);
             LeftZiplineHitterDeployed = true;
             telemetry.log.add("left bumper:deployed");
-        }
-        else if (pad2.left_bumper & LeftZiplineHitterDeployed)
+        } else if (pad2.left_bumper & LeftZiplineHitterDeployed)
         {
             LeftZiplineHitter.setPosition(LEFT_ZIPLINEHITTER_NOTDEPLOYED);
             LeftZiplineHitterDeployed = false;
             telemetry.log.add("left bumper:notdeployed");
-        }
-        else telemetry.log.add("no bumper");
+        } else telemetry.log.add("no bumper");
 
         //The RightZiplineHitter reads from (0-1), which is different than the LeftZiplineHitter(0-360)
         if (pad2.right_bumper & !RightZiplineHitterDeployed)
         {
             RightZiplineHitter.setPosition(RIGHT_ZIPLINEHITTER_DEPLOYED);
             RightZiplineHitterDeployed = true;
-        }
-        else if (pad2.right_bumper & RightZiplineHitterDeployed)
+        } else if (pad2.right_bumper & RightZiplineHitterDeployed)
         {
             RightZiplineHitter.setPosition(RIGHT_ZIPLINEHITTER_NOTDEPLOYED);
             RightZiplineHitterDeployed = false;
         }
         //toggle field driving mode
-        if (pad1.a )
+        if (pad1.a)
         {
             setFieldDrivingMode();
         }
         //toggle "ready" mode for getting ready to climb the ramp
         //need to drive backwards so we can line up against the ramp
-        else if (pad1.b) {
+        else if (pad1.b)
+        {
             setBackwardsDriveMode();
         }
         //toggle drive climb mode
@@ -369,40 +354,33 @@ public class Synch6220TeleOp extends SynchronousOpMode
         if (pad1.right_bumper)
         {
             currentDrivePowerFactor = LOW_POWER;
-        }
-        else
+        } else
         {
             currentDrivePowerFactor = FULL_POWER;
         }
-
     }
 
     //This is the driving mode for going up the ramp
-    private void setRampClimbingMode()
-    {
+    private void setRampClimbingMode() {
         setDriveMode(DriveModeEnum.DriveModeRamp);
     }
 
     //This is the driving mode we use when we want to drive backwards to align with the ramp
-    private void setBackwardsDriveMode()
-    {
+    private void setBackwardsDriveMode() {
         setDriveMode(DriveModeEnum.DriveModeBackwards);
     }
 
     //This is the driving mode we use when driving around the field
-    private void setFieldDrivingMode()
-    {
+    private void setFieldDrivingMode() {
         setDriveMode(DriveModeEnum.DriveModeField);
     }
 
-    private void setDriveMode(DriveModeEnum mode)
-    {
+    private void setDriveMode(DriveModeEnum mode) {
         currentDriveMode = mode;
     }
 
 
-    void configureDashboard()
-    {
+    void configureDashboard() {
 
         this.telemetry.log.setDisplayOldToNew(false);   // And we show the log in new to old order, just because we want to
         this.telemetry.log.setCapacity(10);             // We can control the number of lines used by the log
@@ -410,19 +388,15 @@ public class Synch6220TeleOp extends SynchronousOpMode
         // Configure the dashboard. Here, it will have one line, which will contain three items
         this.telemetry.addLine
                 (
-                        this.telemetry.item("left back:", new IFunc<Object>()
-                        {
+                        this.telemetry.item("left back:", new IFunc<Object>() {
                             @Override
-                            public Object value()
-                            {
+                            public Object value() {
                                 return format(MotorLeftBack.getPower());
                             }
                         }),
-                        this.telemetry.item("right back: ", new IFunc<Object>()
-                        {
+                        this.telemetry.item("right back: ", new IFunc<Object>() {
                             @Override
-                            public Object value()
-                            {
+                            public Object value() {
                                 return format(MotorRightBack.getPower());
                             }
                         })
