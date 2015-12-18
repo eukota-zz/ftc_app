b6fcc100d796a33c6d2479737e53de4262005051
Coleson <thenorthernquasar@gmail.com>
2015-12-17 20:57:14 -0800

    Added a test file for writing calibrated color sensor values to a JSON file on the phone

---
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/JSONTest.java
@@ -0,0 +1,20 @@
+package org.usfirst.ftc.exampleteam.yourcodehere;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSONTest extends GlobalRobotAttributes
+{
+    JSONObject colorSensorValueStorage = new JSONObject();
+
+    public void setCalibratedBlue() throws JSONException
+    {
+        colorSensorValueStorage.put("Calibrated Blue", colorSensorBeacon.blue());
+        calibratedBlue = colorSensorBeacon.blue();
+    }
+
+    public int receiveCalibratedBlue() throws JSONException
+    {
+        return colorSensorValueStorage.getInt("Calibrated Blue");
+    }
+}
