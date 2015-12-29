3550d9e0ceec165c1866010ca76fecfec1e925b5
Cole <Cole>
2015-12-28 17:39:14 -0800

    I renamed BlueSideAuto to 6220BlueSideAuto and made it inherit from Master6220OpMode, adding to our inheritance structure.  This simplified our autonomous, but it is still messy and needs to be generalized for both red side and blue side cases(including generalizing the program name itself.)

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/BlueSideAuto6220.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/BlueSideAuto6220.java
@@ -17,19 +17,14 @@ import java.util.concurrent.TimeUnit;
 /**
  * An example of a synchronous opmode that implements a simple drive-a-bot.
  */
-@TeleOp(name = "BlueSideAuto", group = "Swerve Examples")
-public class BlueSideAuto extends SynchronousOpMode
+@TeleOp(name = "BlueSideAuto6220", group = "Swerve Examples")
+//TODO make variables to implement a red and blue side auto in the same program and simplify cluttered functions
+public class BlueSideAuto6220 extends Master6220OpMode
 {
-    // All hardware variables can only be initialized inside the main() function,
-    // not here at their member variable declarations.
-    DcMotor MotorRightBack = null;
-    DcMotor MotorLeftBack = null;
-    DcMotor MotorLeftTriangle = null;
-    DcMotor MotorRightTriangle = null;
-    DcMotor MotorLeftClimber = null;
-    DcMotor MotorRightClimber = null;
-
-
+    public BlueSideAuto6220()
+    {
+        super();
+    }
     // Our sensors, motors, and other devices go here, along with other long term state
     IBNO055IMU imu;
     ElapsedTime elapsed = new ElapsedTime();
@@ -48,34 +43,6 @@ public class BlueSideAuto extends SynchronousOpMode
     @Override
     protected void main() throws InterruptedException
     {
-        // Initialize our hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names you assigned during the robot configuration
-        // step you did in the FTC Robot Controller app on the phone.
-        this.MotorRightBack = this.hardwareMap.dcMotor.get("MotorRightBack");
-        this.MotorLeftBack = this.hardwareMap.dcMotor.get("MotorLeftBack");
-        this.MotorLeftTriangle = this.hardwareMap.dcMotor.get("MotorLeftTriangle");
-        this.MotorRightTriangle = this.hardwareMap.dcMotor.get("MotorRightTriangle");
-        this.MotorLeftClimber = this.hardwareMap.dcMotor.get("MotorLeftClimber");
-        this.MotorRightClimber = this.hardwareMap.dcMotor.get("MotorRightClimber");
-
-        // Configure the knobs of the hardware according to how you've wired your
-        // robot. Here, we assume that there are no encoders connected to the motors,
-        // so we inform the motor objects of that fact.
-        this.MotorRightBack.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorLeftBack.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorLeftTriangle.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorLeftClimber.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorRightClimber.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        this.MotorRightTriangle.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-
-        // One of the two motors (here, the left) should be set to reversed direction
-        // so that it can take the same power level values as the other motor.
-        this.MotorRightBack.setDirection(DcMotor.Direction.REVERSE);
-        this.MotorRightTriangle.setDirection(DcMotor.Direction.REVERSE);
-        this.MotorRightClimber.setDirection(DcMotor.Direction.REVERSE);
-        //this.MotorLeftTriangle.setDirection(DcMotor.Direction.REVERSE);
-        //this.MotorLeftClimber.setDirection(DcMotor.Direction.REVERSE);
-
         // We are expecting the IMU to be attached to an I2C port on  a core device interface
         // module and named "imu". Retrieve that raw I2cDevice and then wrap it in an object that
         // semantically understands this particular kind of sensor.
@@ -97,15 +64,14 @@ public class BlueSideAuto extends SynchronousOpMode
 
         //autonomous code starts here
         Thread.sleep(100);
-        drive(1);
+        driveStraight(1);
         Thread.sleep(2000);
-        halt();
+        this.stopAllMotors();
         turnRight(45, .5);
-
-        halt();
-        drive(1);
+        this.stopAllMotors();
+        driveStraight(1);
         Thread.sleep(2000);
-        halt();
+        this.stopAllMotors();
 
         /*
         driveWithIMU(90,2000,0.02);
@@ -114,29 +80,10 @@ public class BlueSideAuto extends SynchronousOpMode
         */
     }
 
-    void halt() {
-        drive(0);
-    }
-
-    void drive(double power) {
-        drive(power, power);
-        // Tell the motors
-//        this.MotorRightBack.setPower(power);
-//        this.MotorLeftClimber.setPower(0);
-//        this.MotorRightClimber.setPower(0);
-//        //this.MotorRightTriangle.setPower(power1);
-//        this.MotorLeftBack.setPower(power);
-//        //this.MotorLeftTriangle.setPower(power2);
-    }
-
-    void drive(double leftPower, double rightPower) {
-        // Tell the motors
-        this.MotorRightBack.setPower(rightPower);
-        this.MotorLeftClimber.setPower(0);
-        this.MotorRightClimber.setPower(0);
-        this.MotorRightTriangle.setPower(rightPower);
-        this.MotorLeftBack.setPower(leftPower);
-        this.MotorLeftTriangle.setPower(leftPower);
+    void driveStraight(double power)
+    {
+        //use previous function to simplify driving in a straight line
+        driveForwards(power, power, power, power);
     }
 
     // unused function - consider removing?
@@ -148,13 +95,15 @@ public class BlueSideAuto extends SynchronousOpMode
 
     // @todo change to use modulus. The percent symbol is modulus (ie: 365%360 = 5) Could do "return abs(angle%360)"
     // @todo needs description of how to use
-    double normalizeAngle360(double angle) {
+    double normalizeAngle360(double angle)
+    {
         while (angle >= 360) angle -= 360;
         while (angle < 0) angle += 360;
         return angle;
     }
 
-    double getCurrentHeading() {
+    double getCurrentHeading()
+    {
         angles = imu.getAngularOrientation();
 
         // Emit telemetry with the freshest possible values
@@ -164,33 +113,42 @@ public class BlueSideAuto extends SynchronousOpMode
     }
 
     //NOT NECESSARY, deprecated  @todo replace usage with java sign function
-    int sign(double value){
-        if(value == 0){
+    int sign(double value)
+    {
+        if(value == 0)
+        {
             return 0;
         }
-        else{
+        else
+        {
             return (int) (Math.abs(value)/value);
         }
     }
     // @todo we can't understand what inputs are allowed for faceAngle
-    void driveWithIMU(double faceAngle, int time, double reactFactor) throws InterruptedException {
+    void driveWithIMU(double faceAngle, int time, double reactFactor) throws InterruptedException
+    {
         int sinceStart = 0;
         double delta;
-        while (sinceStart < time){
+        while (sinceStart < time)
+        {
             delta = faceAngle - getCurrentHeading(); // angle is in degrees
-            if (delta < 0){
-                drive(1+delta*reactFactor, 1); // drive power is -1.0 to 1.0. But delta here is in degrees so this is going to be odd
+            if (delta < 0)
+            {
+                driveForwards(1 + delta*reactFactor, 1, 1 + delta*reactFactor, 1);
+                // drive power is -1.0 to 1.0. But delta here is in degrees so this is going to be odd
             }
-            else if (delta > 0){
-                drive(1, 1-delta*reactFactor);
+            else if (delta > 0)
+            {
+                driveForwards(1, 1 - delta*reactFactor, 1, 1 - delta*reactFactor);
             }
             sinceStart++;
             Thread.sleep(1);
         }
-        halt();
+        this.stopAllMotors();
     }
     //turn the robot to an angle relative to it's starting position before this is called
     //@miyu you could read the init heading at the start so that you know what direction is forward.
+    //TODO this method is full of unorganised code, confusing functions, and magic numbers; it needs to be more understandable
     void turnBotTo(double faceAngle, boolean pid){
         //array of last 10 diffs
         double[] delta = {0,0,0,0,0,0,0,0,0,0};
@@ -215,19 +173,19 @@ public class BlueSideAuto extends SynchronousOpMode
             }
             //move the age stack down
             for (int i = 0; i < 9; i++){
-                delta[i+1] = delta[i];
+                delta[i + 1] = delta[i];
             }
             delta[0] = faceAngle - getCurrentHeading() - spinOffset;
             sum += delta[0];
             direction = sign(delta[0]);
-            drive(-1*direction,direction);
+            driveForwards(-1 * direction, direction, -1 * direction, direction);
         }
-        halt();
+        this.stopAllMotors();
     }
 
     void turnRight(double delta, double power)
     {
-        turnLeft(-delta,power);
+        turnLeft(-delta, power);
     }
     void turnLeft(double delta, double power) {
         //richTextBoxResult.Text = "";
@@ -276,12 +234,14 @@ public class BlueSideAuto extends SynchronousOpMode
             //it means that the compass will pass 0 (going in the negative direction) on the way to the dest.
             //When that happens, the current heading will jump from 0 to 359.
             //We will offset our angles to ensure that we don't have that problem.
-            if (cur < dest) {
+            if (cur < dest)
+            {
                 offset = (360 - dest) + GUARD; //put dest at GUARD, move cur by the same amount
             }
             //We won't cross 0 in this turn, but we still want to put some space between
             //the start/end angles and the discontinuity at 0/360
-            else {
+            else
+            {
                 offset = (GUARD - dest); //put dest at GUARD, move cur by the same amount
             }
 
@@ -290,9 +250,10 @@ public class BlueSideAuto extends SynchronousOpMode
             cur = normalizeAngle360(cur + offset);
             dest = normalizeAngle360(dest + offset);
 
-            drive(power, -power);
+            driveForwards(power, -power, power, -power);
 
-            while ((cur > dest)) {
+            while ((cur > dest))
+            {
                 //going left = --
                 cur = normalizeAngle360(getCurrentHeading() + offset);
                 //watchdog++;
@@ -312,7 +273,7 @@ public class BlueSideAuto extends SynchronousOpMode
             cur = normalizeAngle360(cur + offset);
             dest = normalizeAngle360(dest + offset);
 
-            drive(-power, power);
+            driveForwards(-power, power, -power, power);
 
             //now compass 0 doesn't fall between the current heading and the destination,
             //so we can use a < comparison to wait for our target
@@ -325,38 +286,6 @@ public class BlueSideAuto extends SynchronousOpMode
         }
 
         //result += "dest reached: " + cur.ToString() + "\n";
-        halt();
+        this.stopAllMotors();
     }
-
-    void configureDashboard() {
-        // Configure the dashboard. Here, it will have one line, which will contain three items
-        this.telemetry.addLine
-                (
-                        this.telemetry.item("left:", new IFunc<Object>() {
-                            @Override
-                            public Object value() {
-                                return format(MotorLeftBack.getPower());
-                            }
-                        }),
-                        this.telemetry.item("right: ", new IFunc<Object>() {
-                            @Override
-                            public Object value() {
-                                return format(MotorRightBack.getPower());
-                            }
-                        })
-                        /*this.telemetry.item("mode: ", new IFunc<Object>() {
-                            @Override
-                            public Object value() {
-                                return MotorRightBack.getChannelMode();
-                            }
-                        })*/
-                );
-    }
-
-    // Handy functions for formatting data for the dashboard
-    String format(double d) {
-        return String.format("%.1f", d);
-    }
-
-
 }
