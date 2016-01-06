54e8011c1e01761c3447b57767aa60d1b61fe44f
Coleson <thenorthernquasar@gmail.com>
2016-01-05 21:01:20 -0800

    Added a file for testing IMU's for angular movement

---
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/IMUTest.java
@@ -0,0 +1,57 @@
+package org.usfirst.ftc.exampleteam.yourcodehere;
+
+import org.swerverobotics.library.ClassFactory;
+import org.swerverobotics.library.SynchronousOpMode;
+import org.swerverobotics.library.interfaces.Disabled;
+import org.swerverobotics.library.interfaces.IBNO055IMU;
+import org.swerverobotics.library.interfaces.IFunc;
+import org.swerverobotics.library.interfaces.TeleOp;
+
+/**
+ * Testing IMU's and having fun doing it
+ */
+@TeleOp(name="IMUTest")
+public class IMUTest extends SynchronousOpMode
+{
+    IBNO055IMU imu;
+    IBNO055IMU.Parameters   parameters = new IBNO055IMU.Parameters();
+
+    double currentHeading;
+
+    @Override protected void main() throws InterruptedException
+    {
+        parameters.angleunit      = IBNO055IMU.ANGLEUNIT.DEGREES;
+        imu                       = ClassFactory.createAdaFruitBNO055IMU(hardwareMap.i2cDevice.get("imu"), parameters);
+
+        configDash();
+        telemetry.setUpdateIntervalMs(30);
+
+        waitForStart();
+
+        double calibratedHeading = imu.getAngularOrientation().heading;
+
+        while(opModeIsActive())
+        {
+            currentHeading = imu.getAngularOrientation().heading - calibratedHeading;
+
+            if(currentHeading < 0)
+                currentHeading += 360;
+
+            telemetry.update();
+            idle();
+        }
+    }
+
+    public void configDash()
+    {
+        telemetry.addLine(
+            telemetry.item("heading: ", new IFunc<Object>()
+            {
+                public Object value()
+                {
+                    return currentHeading;
+                }
+            }));
+
+    }
+}
