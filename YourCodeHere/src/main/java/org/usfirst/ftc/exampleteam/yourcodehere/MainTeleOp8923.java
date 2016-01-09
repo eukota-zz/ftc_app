e4241be5510c7af6b93e69845b17e2247698b43f
Fromen <drywdude@yahoo.com>
2016-01-09 11:36:56 -0800

    Moved gamepad based movements into methods in MasterTeleOp, and made MainTeleOp use them

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/MainTeleOp8923.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/MainTeleOp8923.java
@@ -12,40 +12,7 @@ public class MainTeleOp8923 extends Master8923TeleOp
     @Override protected void main() throws InterruptedException
     {
         robotInit();
-
-        // Left drive motor info
-        telemetry.addLine
-                (
-                        this.telemetry.item("Left Power:", new IFunc<Object>() {
-                            @Override
-                            public Object value() {
-                                return motorLeft.getPower();
-                            }
-                        }),
-                        this.telemetry.item("Left Position: ", new IFunc<Object>() {
-                            @Override
-                            public Object value() {
-                                return motorLeft.getCurrentPosition();
-                            }
-                        })
-                );
-
-        // Right drive motor info
-        telemetry.addLine
-                (
-                        this.telemetry.item("Right Power: ", new IFunc<Object>() {
-                            @Override
-                            public Object value() {
-                                return motorRight.getPower();
-                            }
-                        }),
-                        this.telemetry.item("Right Position: ", new IFunc<Object>() {
-                            @Override
-                            public Object value() {
-                                return motorRight.getCurrentPosition();
-                            }
-                        })
-                );
+        configureTelemtry();
 
         // Wait for the game to begin
         this.waitForStart();
@@ -67,103 +34,17 @@ public class MainTeleOp8923 extends Master8923TeleOp
 
     public void driver1Controls()
     {
-        // Tank drive based on joysticks of controller 1
-        if(gamepad1.y)
-        {
-            slowModeFactor = 2.5;
-        }
-        else if(gamepad1.a)
-        {
-            slowModeFactor = 1.0;
-        }
-
-        // Motors aren't even, so only right motor needs power reduction
-        motorLeft.setPower(gamepad1.left_stick_y);
-        motorRight.setPower(gamepad1.right_stick_y / slowModeFactor);
-
-        // Tape Measure of Doom extension based on controller 1
-        if(gamepad1.left_trigger > 0)
-        {
-            motorTapeMeasure.setPower(-POWER_FULL);
-        }
-        else if(gamepad1.right_trigger > 0)
-        {
-            motorTapeMeasure.setPower(POWER_FULL);
-        }
-        else
-        {
-            motorTapeMeasure.setPower(POWER_STOP);
-        }
-
-        // Tape Measure of Doom elevation based on controller 1
-        if(gamepad1.left_bumper && servoTapeMeasureElevation.getPosition() <= 1 - TAPE_MEASURE_ELEVATION_RATE)
-        {
-            servoTapeMeasureElevation.setPosition(servoTapeMeasureElevation.getPosition() + TAPE_MEASURE_ELEVATION_RATE);
-        }
-        else if(gamepad1.right_bumper && servoTapeMeasureElevation.getPosition() >= 0 + TAPE_MEASURE_ELEVATION_RATE)
-        {
-            servoTapeMeasureElevation.setPosition(servoTapeMeasureElevation.getPosition() - TAPE_MEASURE_ELEVATION_RATE);
-        }
+        tankDrive(gamepad1);
+        controlTapeMeasureMotors(gamepad1);
+        controlTapeMeasureServo(gamepad1);
     }
 
     public void driver2Controls() throws InterruptedException
     {
-        // Move collector based on triggers on controller 2 if the bottom isn't up
-        if(gamepad2.right_trigger > 0 && !collectorHingeIsUp)
-            motorCollector.setPower(gamepad2.right_trigger);
-        else if(gamepad2.left_trigger > 0 && !collectorHingeIsUp)
-            motorCollector.setPower(-gamepad2.left_trigger);
-        else
-            motorCollector.setPower(POWER_STOP);
-
-        // Move scorer based on D-pad on controller 2
-        if(gamepad2.dpad_left)
-            motorScorer.setPower(POWER_SCORER);
-        else if(gamepad2.dpad_right)
-            motorScorer.setPower(-POWER_SCORER);
-        else
-            motorScorer.setPower(POWER_STOP);
-
-        // Move zipline servos based on left and right bumpers
-        if (gamepad2.left_bumper)
-        {
-            ziplineLeftIsOut = !ziplineLeftIsOut;
-            if(ziplineLeftIsOut)
-                servoLeftZipline.setPosition(ZIPLINE_LEFT_OUT);
-            else
-                servoLeftZipline.setPosition(ZIPLINE_LEFT_UP);
-        }
-        if(gamepad2.right_bumper)
-        {
-            ziplineRightIsOut = !ziplineRightIsOut;
-            if (ziplineRightIsOut)
-                servoRightZipline.setPosition(ZIPLINE_RIGHT_OUT);
-            else
-                servoRightZipline.setPosition(ZIPLINE_RIGHT_UP);
-        }
-
-        // Move collector ramp up and down based on x and y
-        if(gamepad2.y)
-        {
-            collectorHingeIsUp = true;
-            motorCollector.setPower(POWER_STOP);
-            Thread.sleep(250);
-            servoCollectorHinge.setPosition(COLLECTOR_HINGE_UP);
-        }
-        else if(gamepad2.x)
-        {
-            collectorHingeIsUp = false;
-            servoCollectorHinge.setPosition(COLLECTOR_HINGE_DOWN);
-        }
-
-        // Toggle climber dumper servo position
-        if (gamepad2.a)
-        {
-            climberArmOut = !climberArmOut;
-            if(climberArmOut)
-                servoClimberDumper.setPosition(CLIMBER_ARM_OUT);
-            else
-                servoClimberDumper.setPosition(CLIMBER_ARM_IN);
-        }
+        controlCollector(gamepad2);
+        controlScorer(gamepad2);
+        controlZiplineServos(gamepad2);
+        controlCollectorRamp(gamepad2);
+        controlClimberDumper(gamepad2);
     }
 }
--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/Master8923TeleOp.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/Master8923TeleOp.java
@@ -1,6 +1,7 @@
 package org.usfirst.ftc.exampleteam.yourcodehere;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /*
@@ -8,5 +9,124 @@ import com.qualcomm.robotcore.hardware.Servo;
  */
 public class Master8923TeleOp extends Master8923
 {
+    public void tankDrive(Gamepad gamepad)
+    {
+        // Tank drive based on joysticks of controller 1
 
+        // Enables slow mode
+        if(gamepad.y)
+        {
+            slowModeFactor = 2.5;
+        }
+        else if(gamepad.a)
+        {
+            slowModeFactor = 1.0;
+        }
+
+        // Motors aren't even, so only right motor needs power reduction
+        motorLeft.setPower(gamepad.left_stick_y);
+        motorRight.setPower(gamepad.right_stick_y / slowModeFactor);
+    }
+
+    public void controlTapeMeasureMotors(Gamepad gamepad)
+    {
+        // Tape Measure of Doom extension based on controller 1
+        if(gamepad.left_trigger > 0)
+        {
+            motorTapeMeasure.setPower(-POWER_FULL);
+        }
+        else if(gamepad.right_trigger > 0)
+        {
+            motorTapeMeasure.setPower(POWER_FULL);
+        }
+        else
+        {
+            motorTapeMeasure.setPower(POWER_STOP);
+        }
+    }
+
+    public void controlTapeMeasureServo(Gamepad gamepad)
+    {
+        // Tape Measure of Doom elevation based on controller 1
+        if(gamepad.left_bumper && servoTapeMeasureElevation.getPosition() <= 1 - TAPE_MEASURE_ELEVATION_RATE)
+        {
+            servoTapeMeasureElevation.setPosition(servoTapeMeasureElevation.getPosition() + TAPE_MEASURE_ELEVATION_RATE);
+        }
+        else if(gamepad.right_bumper && servoTapeMeasureElevation.getPosition() >= 0 + TAPE_MEASURE_ELEVATION_RATE)
+        {
+            servoTapeMeasureElevation.setPosition(servoTapeMeasureElevation.getPosition() - TAPE_MEASURE_ELEVATION_RATE);
+        }
+    }
+
+    public void controlCollector(Gamepad gamepad)
+    {
+        // Move collector based on triggers on controller 2 if the bottom isn't up
+        if(gamepad.right_trigger > 0 && !collectorHingeIsUp)
+            motorCollector.setPower(gamepad.right_trigger);
+        else if(gamepad.left_trigger > 0 && !collectorHingeIsUp)
+            motorCollector.setPower(-gamepad.left_trigger);
+        else
+            motorCollector.setPower(POWER_STOP);
+    }
+
+    public void controlScorer(Gamepad gamepad)
+    {
+        // Move scorer based on D-pad on controller 2
+        if(gamepad.dpad_left)
+            motorScorer.setPower(POWER_SCORER);
+        else if(gamepad.dpad_right)
+            motorScorer.setPower(-POWER_SCORER);
+        else
+            motorScorer.setPower(POWER_STOP);
+    }
+
+    public void controlZiplineServos(Gamepad gamepad)
+    {
+        // Toggle zipline servos based on left and right bumpers
+        if (gamepad.left_bumper)
+        {
+            ziplineLeftIsOut = !ziplineLeftIsOut;
+            if(ziplineLeftIsOut)
+                servoLeftZipline.setPosition(ZIPLINE_LEFT_OUT);
+            else
+                servoLeftZipline.setPosition(ZIPLINE_LEFT_UP);
+        }
+        if(gamepad.right_bumper)
+        {
+            ziplineRightIsOut = !ziplineRightIsOut;
+            if (ziplineRightIsOut)
+                servoRightZipline.setPosition(ZIPLINE_RIGHT_OUT);
+            else
+                servoRightZipline.setPosition(ZIPLINE_RIGHT_UP);
+        }
+    }
+
+    public void controlCollectorRamp(Gamepad gamepad)
+    {
+        // Move collector ramp up and down based on x and y
+        if(gamepad.y)
+        {
+            collectorHingeIsUp = true;
+            motorCollector.setPower(POWER_STOP);
+            servoCollectorHinge.setPosition(COLLECTOR_HINGE_UP);
+        }
+        else if(gamepad.x)
+        {
+            collectorHingeIsUp = false;
+            servoCollectorHinge.setPosition(COLLECTOR_HINGE_DOWN);
+        }
+    }
+
+    public void controlClimberDumper(Gamepad gamepad)
+    {
+        // Toggle climber dumper servo position
+        if (gamepad.a)
+        {
+            climberArmOut = !climberArmOut;
+            if(climberArmOut)
+                servoClimberDumper.setPosition(CLIMBER_ARM_OUT);
+            else
+                servoClimberDumper.setPosition(CLIMBER_ARM_IN);
+        }
+    }
 }
