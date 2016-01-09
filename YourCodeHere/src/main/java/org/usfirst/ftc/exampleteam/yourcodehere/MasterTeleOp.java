e2e4bbf2780556d4d3fa3db7a38893c4c77acb8c
Cole <Cole>
2016-01-08 20:56:30 -0800

    NEXT MTG:  add variable speeds for the hanger servo and motors.

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/MasterTeleOp.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/MasterTeleOp.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 /*
     This contains joystick nput handling methods and initialization
  */
+//TODO variable speed for the hanger servo and motors
 public abstract class MasterTeleOp extends MasterOpMode
 {
     protected void initialize()
