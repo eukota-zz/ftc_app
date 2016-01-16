920da5f230be8cc78d0c849c4661a6eba117bf1c
Fromen <drywdude@yahoo.com>
2016-01-16 01:46:12 -0800

    Added test file for optical distance sensors

---
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/ODSTest.java
@@ -0,0 +1,61 @@
+package org.usfirst.ftc.exampleteam.yourcodehere;
+
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+import org.swerverobotics.library.SynchronousOpMode;
+import org.swerverobotics.library.interfaces.IFunc;
+import org.swerverobotics.library.interfaces.TeleOp;
+
+/**
+ * Robot starts on blue side, goes to beacon,
+ * presses beacon button, and scores climber
+ */
+@TeleOp(name="ODS Test")
+public class ODSTest extends SynchronousOpMode
+{
+    OpticalDistanceSensor lightSensorFront = null;
+    OpticalDistanceSensor lightSensorBack = null;
+
+    @Override public void main() throws InterruptedException
+    {
+        lightSensorFront = hardwareMap.opticalDistanceSensor.get("lightSensorFront");
+        lightSensorBack = hardwareMap.opticalDistanceSensor.get("lightSensorBack");
+        lightSensorFront.enableLed(true);
+        lightSensorBack.enableLed(true);
+
+        // Set up our dashboard computations
+        composeDashboard();
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            telemetry.update();
+            idle();
+        }
+    }
+
+    void composeDashboard() {
+        telemetry.addLine(
+                telemetry.item("Front: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return formatNumber(lightSensorFront.getLightDetected());
+                    }
+                }),
+                this.telemetry.item("Back: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return formatNumber(lightSensorBack.getLightDetected());
+                    }
+                }));
+
+    }
+
+    public String formatNumber(double number)
+    {
+        return String.format("%.2f", number);
+    }
+
+}
