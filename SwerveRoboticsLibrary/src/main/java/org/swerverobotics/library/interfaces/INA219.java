afa7a8b79a024ebce03600639a746b9e00f52fed
Steve Geffner <steve.geffner.phd@gmail.com>
2016-02-24 20:33:02 -0800

    public, static, final not needed on members of an interface

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/interfaces/INA219.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/interfaces/INA219.java
@@ -85,15 +85,15 @@ public interface INA219
     // Status inquiry
     //----------------------------------------------------------------------------------------------
 
-    public void setCalibration(INA219.Parameters parameters);
+    void setCalibration(INA219.Parameters parameters);
 
-    public double getBusVoltage_V();
+    double getBusVoltage_V();
 
-    public double getShuntVoltage_mV();
+    double getShuntVoltage_mV();
 
-    public double getCurrent_mA();
+    double getCurrent_mA();
 
-    public void resetINA219();
+    void resetINA219();
 
     //----------------------------------------------------------------------------------------------
     // Low level reading and writing 
@@ -187,92 +187,92 @@ public interface INA219
     /*=========================================================================
     I2C ADDRESS/BITS
     -----------------------------------------------------------------------*/
-    public static final int INA219_ADDRESS = (0x40);    // 1000000 (A0+A1=GND)
-    public static final int INA219_READ = (0x01);
+    int INA219_ADDRESS = (0x40);    // 1000000 (A0+A1=GND)
+    int INA219_READ = (0x01);
 /*=========================================================================*/
 
     /*=========================================================================
         CONFIG REGISTER (R/W)
         -----------------------------------------------------------------------*/
-    public static final int INA219_REG_CONFIG = (0x00);
+    int INA219_REG_CONFIG = (0x00);
     /*---------------------------------------------------------------------*/
-    public static final int INA219_CONFIG_RESET = (0x8000);  // Reset Bit
+    int INA219_CONFIG_RESET = (0x8000);  // Reset Bit
 
     enum VOLTAGE_RANGE { VOLTS_16 (0x0000), VOLTS_32 (0x2000); public final byte bVal; VOLTAGE_RANGE(int i) { bVal =(byte)i; }}
-    public static final int INA219_CONFIG_BVOLTAGERANGE_MASK = (0x2000);  // Bus Voltage Range Mask
-    public static final int INA219_CONFIG_BVOLTAGERANGE_16V = (0x0000); // 0-16V Range
-    public static final int INA219_CONFIG_BVOLTAGERANGE_32V = (0x2000);// 0-32V Range
+    int INA219_CONFIG_BVOLTAGERANGE_MASK = (0x2000);  // Bus Voltage Range Mask
+    int INA219_CONFIG_BVOLTAGERANGE_16V = (0x0000); // 0-16V Range
+    int INA219_CONFIG_BVOLTAGERANGE_32V = (0x2000);// 0-32V Range
 
     enum GAIN { GAIN_1_40MV (0x0000), GAIN_2_80MV (0x0800), GAIN_4_160MV (0x1000), GAIN_8_320MV (0x1800); public final byte bVal; GAIN(int i) { bVal =(byte)i; }}
-    public static final int INA219_CONFIG_GAIN_MASK = (0x1800);  // Gain Mask
-    public static final int INA219_CONFIG_GAIN_1_40MV = (0x0000);  // Gain 1, 40mV Range
-    public static final int INA219_CONFIG_GAIN_2_80MV = (0x0800);  // Gain 2, 80mV Range
-    public static final int INA219_CONFIG_GAIN_4_160MV = (0x1000);  // Gain 4, 160mV Range
-    public static final int INA219_CONFIG_GAIN_8_320MV = (0x1800);  // Gain 8, 320mV Range
+    int INA219_CONFIG_GAIN_MASK = (0x1800);  // Gain Mask
+    int INA219_CONFIG_GAIN_1_40MV = (0x0000);  // Gain 1, 40mV Range
+    int INA219_CONFIG_GAIN_2_80MV = (0x0800);  // Gain 2, 80mV Range
+    int INA219_CONFIG_GAIN_4_160MV = (0x1000);  // Gain 4, 160mV Range
+    int INA219_CONFIG_GAIN_8_320MV = (0x1800);  // Gain 8, 320mV Range
 
     enum BUS_ADC_RESOLUTION { RESOLUTION_9BIT (0x0080), RESOLUTION_10BIT (0x0100), RESOLUTION_11BIT (0x0200), RESOLUTION_12BIT (0x0400) ; public final byte bVal; BUS_ADC_RESOLUTION(int i) { bVal =(byte)i; }}
-    public static final int INA219_CONFIG_BADCRES_MASK = (0x0780);  // Bus ADC Resolution Mask
-    public static final int INA219_CONFIG_BADCRES_9BIT = (0x0080);  // 9-bit bus res = 0..511
-    public static final int INA219_CONFIG_BADCRES_10BIT = (0x0100);  // 10-bit bus res = 0..1023
-    public static final int INA219_CONFIG_BADCRES_11BIT = (0x0200);  // 11-bit bus res = 0..2047
-    public static final int INA219_CONFIG_BADCRES_12BIT = (0x0400);  // 12-bit bus res = 0..4097
+    int INA219_CONFIG_BADCRES_MASK = (0x0780);  // Bus ADC Resolution Mask
+    int INA219_CONFIG_BADCRES_9BIT = (0x0080);  // 9-bit bus res = 0..511
+    int INA219_CONFIG_BADCRES_10BIT = (0x0100);  // 10-bit bus res = 0..1023
+    int INA219_CONFIG_BADCRES_11BIT = (0x0200);  // 11-bit bus res = 0..2047
+    int INA219_CONFIG_BADCRES_12BIT = (0x0400);  // 12-bit bus res = 0..4097
 
     enum SHUNT_ADC_RESOLUTION { RESOLUTION_9BIT_1S_84US (0x0000), RESOLUTION_10BIT_1S_148US (0x0008), RESOLUTION_11BIT_1S_276US (0x0010), RESOLUTION_12BIT_1S_532US (0x0018),
                                 RESOLUTION_12BIT_2S_1060US (0x000048); public final byte bVal; SHUNT_ADC_RESOLUTION(int i) { bVal =(byte)i; } }
-    public static final int INA219_CONFIG_SADCRES_MASK = (0x0078);  // Shunt ADC Resolution and Averaging Mask
-    public static final int INA219_CONFIG_SADCRES_9BIT_1S_84US = (0x0000);  // 1 x 9-bit shunt sample
-    public static final int INA219_CONFIG_SADCRES_10BIT_1S_148US = (0x0008);  // 1 x 10-bit shunt sample
-    public static final int INA219_CONFIG_SADCRES_11BIT_1S_276US = (0x0010);  // 1 x 11-bit shunt sample
-    public static final int INA219_CONFIG_SADCRES_12BIT_1S_532US = (0x0018);  // 1 x 12-bit shunt sample
-    public static final int INA219_CONFIG_SADCRES_12BIT_2S_1060US = (0x0048);     // 2 x 12-bit shunt samples averaged together
-    public static final int INA219_CONFIG_SADCRES_12BIT_4S_2130US = (0x0050);  // 4 x 12-bit shunt samples averaged together
-    public static final int INA219_CONFIG_SADCRES_12BIT_8S_4260US = (0x0058);  // 8 x 12-bit shunt samples averaged together
-    public static final int INA219_CONFIG_SADCRES_12BIT_16S_8510US = (0x0060);  // 16 x 12-bit shunt samples averaged together
-    public static final int INA219_CONFIG_SADCRES_12BIT_32S_17MS = (0x0068);  // 32 x 12-bit shunt samples averaged together
-    public static final int INA219_CONFIG_SADCRES_12BIT_64S_34MS = (0x0070);  // 64 x 12-bit shunt samples averaged together
-    public static final int INA219_CONFIG_SADCRES_12BIT_128S_69MS = (0x0078);  // 128 x 12-bit shunt samples averaged together
+    int INA219_CONFIG_SADCRES_MASK = (0x0078);  // Shunt ADC Resolution and Averaging Mask
+    int INA219_CONFIG_SADCRES_9BIT_1S_84US = (0x0000);  // 1 x 9-bit shunt sample
+    int INA219_CONFIG_SADCRES_10BIT_1S_148US = (0x0008);  // 1 x 10-bit shunt sample
+    int INA219_CONFIG_SADCRES_11BIT_1S_276US = (0x0010);  // 1 x 11-bit shunt sample
+    int INA219_CONFIG_SADCRES_12BIT_1S_532US = (0x0018);  // 1 x 12-bit shunt sample
+    int INA219_CONFIG_SADCRES_12BIT_2S_1060US = (0x0048);     // 2 x 12-bit shunt samples averaged together
+    int INA219_CONFIG_SADCRES_12BIT_4S_2130US = (0x0050);  // 4 x 12-bit shunt samples averaged together
+    int INA219_CONFIG_SADCRES_12BIT_8S_4260US = (0x0058);  // 8 x 12-bit shunt samples averaged together
+    int INA219_CONFIG_SADCRES_12BIT_16S_8510US = (0x0060);  // 16 x 12-bit shunt samples averaged together
+    int INA219_CONFIG_SADCRES_12BIT_32S_17MS = (0x0068);  // 32 x 12-bit shunt samples averaged together
+    int INA219_CONFIG_SADCRES_12BIT_64S_34MS = (0x0070);  // 64 x 12-bit shunt samples averaged together
+    int INA219_CONFIG_SADCRES_12BIT_128S_69MS = (0x0078);  // 128 x 12-bit shunt samples averaged together
 
     enum CONFIG_MODE { POWERDOWN(0x0000), SVOLT_TRIGGERED(0x0001), BVOLT_TRIGGERED(0x0002), SANDBVOLTTRIGGERED(0x0003),
                        ADC_OFF(0x0004), SVOLT_CONTINUOUS(0x0005), BVOLT_CONTINUOUS(0x0006), SANDBVOLT_CONTINUOUS(0x0007); public final byte bVal; CONFIG_MODE(int i) { bVal =(byte)i; }}
-    public static final int INA219_CONFIG_MODE_MASK = (0x0007);  // Operating Mode Mask
-    public static final int INA219_CONFIG_MODE_POWERDOWN = (0x0000);
-    public static final int INA219_CONFIG_MODE_SVOLT_TRIGGERED = (0x0001);
-    public static final int INA219_CONFIG_MODE_BVOLT_TRIGGERED = (0x0002);
-    public static final int INA219_CONFIG_MODE_SANDBVOLT_TRIGGERED = (0x0003);
-    public static final int INA219_CONFIG_MODE_ADCOFF = (0x0004);
-    public static final int INA219_CONFIG_MODE_SVOLT_CONTINUOUS = (0x0005);
-    public static final int INA219_CONFIG_MODE_BVOLT_CONTINUOUS = (0x0006);
-    public static final int INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS = (0x0007);
+    int INA219_CONFIG_MODE_MASK = (0x0007);  // Operating Mode Mask
+    int INA219_CONFIG_MODE_POWERDOWN = (0x0000);
+    int INA219_CONFIG_MODE_SVOLT_TRIGGERED = (0x0001);
+    int INA219_CONFIG_MODE_BVOLT_TRIGGERED = (0x0002);
+    int INA219_CONFIG_MODE_SANDBVOLT_TRIGGERED = (0x0003);
+    int INA219_CONFIG_MODE_ADCOFF = (0x0004);
+    int INA219_CONFIG_MODE_SVOLT_CONTINUOUS = (0x0005);
+    int INA219_CONFIG_MODE_BVOLT_CONTINUOUS = (0x0006);
+    int INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS = (0x0007);
 /*=========================================================================*/
 
     /*=========================================================================
         SHUNT VOLTAGE REGISTER (R)
         -----------------------------------------------------------------------*/
-    public static final int INA219_REG_SHUNTVOLTAGE = (0x01);
+    int INA219_REG_SHUNTVOLTAGE = (0x01);
 /*=========================================================================*/
 
     /*=========================================================================
         BUS VOLTAGE REGISTER (R)
         -----------------------------------------------------------------------*/
-    public static final int INA219_REG_BUSVOLTAGE = (0x02);
+    int INA219_REG_BUSVOLTAGE = (0x02);
 /*=========================================================================*/
 
     /*=========================================================================
         POWER REGISTER (R)
         -----------------------------------------------------------------------*/
-    public static final int INA219_REG_POWER = (0x03);
+    int INA219_REG_POWER = (0x03);
 /*=========================================================================*/
 
     /*=========================================================================
         CURRENT REGISTER (R)
         -----------------------------------------------------------------------*/
-    public static final int INA219_REG_CURRENT = (0x04);
+    int INA219_REG_CURRENT = (0x04);
 /*=========================================================================*/
 
     /*=========================================================================
         CALIBRATION REGISTER (R/W)
         -----------------------------------------------------------------------*/
-    public static final int INA219_REG_CALIBRATION = (0x05);
+    int INA219_REG_CALIBRATION = (0x05);
 /*=========================================================================*/
 
 
