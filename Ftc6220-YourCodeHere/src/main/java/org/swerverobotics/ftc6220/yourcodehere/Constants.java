22fa92e2466af39fb5ef6a28560219f209409db6
Cole <Cole>
2016-03-17 21:07:55 -0700

    A small constant adjustment.

--- Ftc6220-YourCodeHere/src/main/java/org/swerverobotics/ftc6220/yourcodehere/Constants.java
+++ Ftc6220-YourCodeHere/src/main/java/org/swerverobotics/ftc6220/yourcodehere/Constants.java
@@ -14,7 +14,7 @@ public class Constants
     public static final double ZIPLINEHITTER_NOTDEPLOYED = -0.85;
     public static final double ZIPLINEHITTER_DEPLOYED = 0.6;
     public static final double HIKER_DROPPER_NOTDEPLOYED = 0.11;
-    public static final double HIKER_DROPPER_DEPLOYED = 0.67;
+    public static final double HIKER_DROPPER_DEPLOYED = 0.69;
     public static final double HANGER_SERVO_NOTDEPLOYED = 0.0;
     public static final double HANGER_SERVO_STOP = 0.5;
     public static final double HANGER_SERVO_DEPLOYED = 1.0;
