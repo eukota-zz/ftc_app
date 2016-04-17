209c20286c25c3ed831723d3b82162743a2d89ac
Steve Geffner <steve.geffner.phd@gmail.com>
2016-04-16 17:13:26 -0700

    At Jean's suggestion, I've added a drivabot opmode that ends the program after 5 minutes to help ensure we give a turn to kids who are waiting.

---
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/DriveABotWithTimer.java
@@ -0,0 +1,161 @@
+package org.swerverobotics.library.examples;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.swerverobotics.library.SynchronousOpMode;
+import org.swerverobotics.library.interfaces.IFunc;
+import org.swerverobotics.library.interfaces.TeleOp;
+
+/**
+ * Program used to control Drive-A-Bots.
+ * This can be a good reference for drive controls.
+ */
+@TeleOp(name="Drive-A-Bot", group="Swerve Examples")
+
+public class DriveABotWithTimer extends SynchronousOpMode
+{
+    DcMotor motorLeft = null;
+    DcMotor motorRight = null;
+
+    ElapsedTime timer;
+
+    static final int TIME_PER_KID_IN_SECONDS = (30); //5 minute turns per kid
+    boolean kidHasTimeLeft = true;
+
+    @Override protected void main() throws InterruptedException
+    {
+        // Initialize hardware and other important things
+        initializeRobot();
+
+        // Wait until start button has been pressed
+        waitForStart();
+
+        timer.reset();
+        timer.startTime();
+
+        // Main loop
+        while(opModeIsActive() && kidHasTimeLeft)
+        {
+            // Gamepads have a new state, so update things that need updating
+            if(updateGamepads())
+            {
+                tankDrive(); //use tank drive. DO NOT change this without talking to Heidi first!!!
+            }
+
+            telemetry.update();
+            idle();
+
+            kidHasTimeLeft = ((TIME_PER_KID_IN_SECONDS - timer.seconds()) > 0);
+        }
+    }
+
+    /*
+     * Controls the robot with two joysticks
+     * Left joystick controls left side
+     * Right joystick controls right side
+     */
+    public void tankDrive()
+    {
+        motorLeft.setPower(gamepad1.left_stick_y);
+        motorRight.setPower(gamepad1.right_stick_y);
+    }
+
+    /*
+     * Controls the robot with a single joystick
+     * Forward and backward on joystick control forward and backward power
+     * Left and right control turning
+     */
+    public void arcadeDrive()
+    {
+        double forwardPower = gamepad1.left_stick_y;
+        double turningPower = gamepad1.left_stick_x;
+
+        double leftPower = forwardPower + turningPower;
+        double rightPower = forwardPower - turningPower;
+
+        motorLeft.setPower(leftPower);
+        motorRight.setPower(rightPower);
+    }
+
+    /*
+     * Controls robot like a racing video game
+     * Right trigger moves robot forward
+     * Left trigger moves robot backward
+     * Left stick for turning
+     */
+    public void gameDrive()
+    {
+        double forwardPower = gamepad1.right_trigger - gamepad1.left_trigger;
+        double turningPower = gamepad1.left_stick_x * 0.5;
+
+        double leftPower = forwardPower - turningPower;
+        double rightPower = forwardPower + turningPower;
+
+        motorLeft.setPower(leftPower);
+        motorRight.setPower(rightPower);
+    }
+
+    public void initializeRobot()
+    {
+        // Initialize motors to be the hardware motors
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+
+        // We're not using encoders, so tell the motor controller
+        motorLeft.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        motorRight.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+
+        // The motors will run in opposite directions, so flip one
+        //THIS IS SET UP FOR TANK MODE WITH OUR CURRENT DRIVABOTS
+        //DON'T CHANGE IT!
+        motorLeft.setDirection(DcMotor.Direction.REVERSE); //DO NOT change without talking to Heidi first!!!
+
+        timer = new ElapsedTime();
+
+        // Set up telemetry data
+        configureDashboard();
+    }
+
+    public void configureDashboard()
+    {
+        telemetry.addLine
+                (
+                        telemetry.item("Power | Left:", new IFunc<Object>()
+                        {
+                            @Override public Object value()
+                            {
+                                return formatNumber(motorLeft.getPower());
+                            }
+                        }),
+                        telemetry.item("Right: ", new IFunc<Object>()
+                        {
+                            @Override public Object value()
+                            {
+                                return formatNumber(motorLeft.getPower());
+                            }
+                        })
+                );
+        telemetry.addLine
+                (
+                        telemetry.item("Time remaining: ", new IFunc<Object>() {
+                            @Override
+                            public Object value() {
+                                return TIME_PER_KID_IN_SECONDS - timer.seconds();
+                            }
+                        }),
+                        telemetry.item("Timer: ", new IFunc<Object>() {
+                            @Override
+                            public Object value() {
+                                return timer.seconds();
+                            }
+                        })
+                );
+    }
+
+    public String formatNumber(double d)
+    {
+        return String.format("%.2f", d);
+    }
+}
