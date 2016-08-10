fb99753bc8727c99b99543ee9b264ffc7ebc7e12
Fromen <drywdude@yahoo.com>
2016-08-09 23:49:28 -0700

    Tested line following for the first time. Added LEDs, changed numbers around, and read from light sensors only once per loop due to a 50 ms read time. Other small changes were made. Still needs lots of adjusting.

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/shared/LineFollowingBotDrywHank.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/shared/LineFollowingBotDrywHank.java
@@ -2,6 +2,8 @@ package org.swerverobotics.library.shared;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.LED;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.swerverobotics.library.ClassFactory;
 import org.swerverobotics.library.SynchronousOpMode;
@@ -24,20 +26,25 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
     DcMotor motorLeft = null;
     DcMotor motorRight = null;
 
-    // Declare light sensors
+    // Declare light sensors and LEDs
     TSL2561LightSensor lightSensorLeft;
     TSL2561LightSensor lightSensorRight;
     AdaFruitTSL2561LightSensor.Parameters parameters = new  AdaFruitTSL2561LightSensor.Parameters();
+    LED ledRight;
+    LED ledLeft;
 
     // TODO: Change these to reasonable values via testing
     // Declare constants
-    static final double MIN_LIGHT_THRESHOLD = 0.1;
-    static final double MAX_LIGHT_THRESHOLD = 0.5;
-    static final double MAX_ERROR = 1.5;
-    static final double BASE_MOTOR_POWER = 0.5;
-    static final double P_CONSTANT = 0.1;
-    static final double I_CONSTANT = 0.1;
-    static final double D_CONSTANT = 0.1;
+    static final double MIN_LIGHT_THRESHOLD = 25;
+    static final double MAX_LIGHT_THRESHOLD = 70;
+    static final double MAX_ERROR = 10;
+    static final double BASE_MOTOR_POWER = 0.1;
+    static final double P_CONSTANT = 0.005;
+    static final double I_CONSTANT = 0.0;
+    static final double D_CONSTANT = 0.0;
+
+    // TODO: Delete this when done testing
+    ElapsedTime time = new ElapsedTime();
 
     // Declare variables
     double error = 0.0;
@@ -48,6 +55,7 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
     double lastPosition = 0.0;
     double currentPosition = 0.0;
     double deltaPosition = 0.0;
+    int lightLeft, lightRight;
 
     @Override protected void main() throws InterruptedException
     {
@@ -58,12 +66,15 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
         waitForStart();
 
         // Main loop
-        while(opModeIsActive() && isOnLine())
+        while(opModeIsActive() /*&& isOnLine()*/)
         {
             // Calculate error to calculate PID to control motors
             updateVariables();
             calculatePID();
             setMotorPower();
+
+            // This is for knowing how long a loop cycle takes and is temporary
+            time.reset();
 
             telemetry.update();
             idle();
@@ -77,23 +88,26 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
     public void updateVariables()
     {
         // Calculate current position based on average of encoders
-        currentPosition = (motorLeft.getCurrentPosition() + motorRight.getCurrentPosition()) / 2;
+        currentPosition = (motorLeft.getCurrentPosition() - motorRight.getCurrentPosition()) / 2;
         deltaPosition = currentPosition - lastPosition;
         lastPosition = currentPosition;
 
         // Used to calculate delta error below
         double lastError = error;
 
+        lightLeft = lightSensorLeft.getLightDetectedRaw();
+        lightRight = lightSensorRight.getLightDetectedRaw();
+
         /*
          * Takes the average of the 2 sensors to find the error.
          * Averaging doesn't work if one sensor is off the line,
          * so a different calculation is made to compensate.
          */
-        error = (lightSensorLeft.getLightDetected() + lightSensorRight.getLightDetected()) / 2;
-        if(lightSensorLeft.getLightDetected() < MIN_LIGHT_THRESHOLD)
-            error = MAX_ERROR - lightSensorRight.getLightDetected();
-        else if(lightSensorRight.getLightDetected() < MIN_LIGHT_THRESHOLD)
-            error = MAX_ERROR - lightSensorLeft.getLightDetected();
+        error = (lightLeft - lightRight) / 2;
+        if(lightLeft < MIN_LIGHT_THRESHOLD)
+            error = -MAX_ERROR; // TODO: Change to a calculation
+        else if(lightRight < MIN_LIGHT_THRESHOLD)
+            error = MAX_ERROR; // TODO: Change to a calculation
 
         // Also calculate error sum and delta error
         errorSum += error;
@@ -108,35 +122,48 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
         /*
          * TODO: Is is necessary to handle this divide by 0 exception?
          * It shouldn't be a problem in normal operation, but might be during the first calculation
+         *
+         * Update 8/9/16: It was an issue, and the try/catch didn't protect it. TODO: Fix / 0
          */
         // Just in case delta time is 0
-        try
+        /*try
         {
             D = D_CONSTANT * deltaError / deltaPosition;
         }
         catch (ArithmeticException e)
         {
             D = 0;
-        }
+        }*/
+
+        PID = P + I + D;
     }
 
     public void setMotorPower()
     {
         // Adjust motor power using PID result
-        motorLeft.setPower(BASE_MOTOR_POWER + PID);
-        motorRight.setPower(BASE_MOTOR_POWER - PID);
+        double leftPow = BASE_MOTOR_POWER - PID;
+        double rightPow = BASE_MOTOR_POWER + PID;
+
+        // Don't exceed 1
+        if(Math.abs(leftPow) > 1)
+            leftPow = Math.signum(leftPow);
+        if(Math.abs(rightPow) > 1)
+            rightPow = Math.signum(rightPow);
+
+        motorLeft.setPower(leftPow);
+        motorRight.setPower(rightPow);
     }
 
     public boolean isOnLine()
     {
         // Check to see if robot has reached the end of the line
-        if(lightSensorLeft.getLightDetected() > MAX_LIGHT_THRESHOLD && lightSensorRight.getLightDetected() > MAX_LIGHT_THRESHOLD)
+        if(lightLeft > MAX_LIGHT_THRESHOLD && lightRight > MAX_LIGHT_THRESHOLD)
         {
             telemetry.log.add("End of the line!");
             return false;
         }
         // Check to see if robot can still see the line
-        else if(lightSensorLeft.getLightDetected() < MIN_LIGHT_THRESHOLD && lightSensorRight.getLightDetected() < MIN_LIGHT_THRESHOLD)
+        else if(lightLeft < MIN_LIGHT_THRESHOLD && lightRight < MIN_LIGHT_THRESHOLD)
         {
             telemetry.log.add("Fell off of the line!");
             return false;
@@ -163,10 +190,18 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
         parameters.gain = TSL2561LightSensor.GAIN.GAIN_16;
         parameters.detectionMode = TSL2561LightSensor.LIGHT_DETECTION_MODE.VISIBLE;
         parameters.integrationTime = TSL2561LightSensor.INTEGRATION_TIME.MS_13;
+        parameters.i2cAddress = TSL2561LightSensor.I2CADDR.DEFAULT;
 
         // Initialize light sensors
-        lightSensorLeft = ClassFactory.createAdaFruitTSL2561LightSensor(hardwareMap.i2cDevice.get("lightSensorLeft"), parameters);
         lightSensorRight = ClassFactory.createAdaFruitTSL2561LightSensor(hardwareMap.i2cDevice.get("lightSensorRight"), parameters);
+        parameters.i2cAddress = TSL2561LightSensor.I2CADDR.ADDR_29; //reusing variable
+        lightSensorLeft = ClassFactory.createAdaFruitTSL2561LightSensor(hardwareMap.i2cDevice.get("lightSensorLeft"), parameters);
+
+        // Initialize LED
+        ledLeft = hardwareMap.led.get("ledLeft");
+        ledRight = hardwareMap.led.get("ledRight");
+        ledLeft.enable(true);
+        ledRight.enable(true);
 
         // Set up telemetry data
         configureDashboard();
@@ -174,6 +209,7 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
 
     public void configureDashboard()
     {
+        telemetry.setUpdateIntervalMs(100);
         // Light sensor readings
         telemetry.addLine
                 (
@@ -181,14 +217,14 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
                         {
                             @Override public Object value()
                             {
-                                return formatNumber(lightSensorLeft.getLightDetected());
+                                return formatNumber(lightLeft);
                             }
                         }),
                         telemetry.item("Right: ", new IFunc<Object>()
                         {
                             @Override public Object value()
                             {
-                                return formatNumber(lightSensorRight.getLightDetected());
+                                return formatNumber(lightRight);
                             }
                         })
                 );
@@ -214,6 +250,13 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
                             @Override public Object value()
                             {
                                 return formatNumber(D);
+                            }
+                        }),
+                        telemetry.item("E: ", new IFunc<Object>()
+                        {
+                            @Override public Object value()
+                            {
+                                return formatNumber(error);
                             }
                         })
                 );
@@ -243,6 +286,16 @@ public class LineFollowingBotDrywHank extends SynchronousOpMode
                             @Override public Object value()
                             {
                                 return formatNumber(deltaPosition);
+                            }
+                        })
+                );
+        telemetry.addLine
+                (
+                        telemetry.item("Time:", new IFunc<Object>()
+                        {
+                            @Override public Object value()
+                            {
+                                return formatNumber(time.milliseconds());
                             }
                         })
                 );
